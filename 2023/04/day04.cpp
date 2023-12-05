e2fc15244c6058165dc551376cc0ac54b58ba48e
TwmMeredydd <49553663+TwmMeredydd@users.noreply.github.com>
2023-12-05 07:25:54 +0000

    2023 Day 4 Part 2 Attempt

--- 2023/04/day04.cpp
+++ 2023/04/day04.cpp
@@ -5,16 +5,66 @@
 using namespace std;
 
 vector<string> input;
+vector<int> matches_per_card;
+
+void calculateMatches() {
+    int num_start_x = 0;
+    while (input[0][num_start_x] != ':') num_start_x++;
+    num_start_x += 2;
+    
+    for (string row : input) {
+        vector<int> winning_nums;
+        int col = num_start_x;
+        matches_per_card.push_back(0);
+        
+        for (; row[col] != '|'; col++) {
+            while (!isdigit(row[col])) col++;
+            if (row[col] == '|') break;
+            winning_nums.push_back(0);
+            while (isdigit(row[col])) {
+                winning_nums.back() *= 10;
+                winning_nums.back() += int(row[col]) - 48;
+                col++;
+            }
+        }
+
+        for (; col < row.length(); col++) {
+            int num = 0;
+            while(!isdigit(row[col])) col++;
+            while(isdigit(row[col])) {
+                num = num*10 + int(row[col])-48;
+                col++;
+            }
+            for (int winning_num : winning_nums) {
+                if (winning_num == num) {
+                    matches_per_card.back() += 1;
+                    break;
+                }
+            }
+        }
+    }
+}
 
 int partOne() {
-    int total_points = 0;
+    int points = 0;
+
+    for (int card : matches_per_card) {
+        if (card == 0) break;
+        points += 1<<(card-1);
+    }
+
+    return points;
+}
+
+int partTwo() {
+    int cardCount = input.size();
     int num_start_x = 0;
     while (input[0][num_start_x] != ':') num_start_x++;
     num_start_x += 2;
 
     for (string row : input) {
         vector<int> winning_nums;
-        float points = 0.5;
+        float matches = 0;
 
         int col = num_start_x;
         
@@ -38,16 +88,14 @@ int partOne() {
             }
             for (int winning_num : winning_nums) {
                 if (winning_num == num) {
-                    points *= 2;
+                    matches += 1;
                     break;
                 }
             }
         }
-
-        total_points += int(points);
     }
 
-    return total_points;
+    return cardCount;
 }
 
 int main() {
@@ -57,6 +105,7 @@ int main() {
     string line;
     if (file.is_open()) {
         while (getline(file, line)) input.push_back(line);
+        calculateMatches();
 
         cout << "Part 1: " << partOne() << endl;
     } else {
