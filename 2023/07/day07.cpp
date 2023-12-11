40ed36c8b5c816688cbf2a1d423cd17716a06d5c
TwmMeredydd <49553663+TwmMeredydd@users.noreply.github.com>
2023-12-11 17:26:42 +0000

    Solve 2023/07 Part 2

--- 2023/07/day07.cpp
+++ 2023/07/day07.cpp
@@ -86,6 +86,76 @@ int partOne() {
     return totalWinnings;
 }
 
+int partTwo() {
+    vector<Hand> hands;
+    char priority[] = {'J', '2','3','4','5','6','7','8','9','T','Q','K','A'};
+    map<char, int> priorityMap;
+    for (int i=0; i<13; i++) priorityMap[priority[i]] = i;
+    
+    for (string strHand : input) {
+        struct Hand hand;
+        for (int i = 0; i<5; i++) hand.cards[i] = strHand[i];
+        for (int i=6; i<strHand.length(); i++) hand.bid = hand.bid*10 + int(strHand[i])-48;
+
+        array<int, 13> counts;
+        counts.fill(0);
+        for (int i=0; i<13; i++) for (char c : hand.cards) if (c == priority[i]) counts[i] += 1;
+
+        int jCount = 0;
+        if (counts[0] > 0) {
+            jCount = counts[0];
+            counts[0] = 0;
+        }
+
+        sort(counts.begin(), counts.end());
+        counts[12] += jCount;
+
+        switch (counts[12]) {
+            case 5:
+                hand.type = FIVE_OF_A_KIND;
+                break;
+            case 4:
+                hand.type = FOUR_OF_A_KIND;
+                break;
+            case 3:
+                hand.type = counts[11] == 2 ? FULL_HOUSE : THREE_OF_A_KIND;
+                break;
+            case 2:
+                hand.type = counts[11] == 2 ? TWO_PAIR : ONE_PAIR;
+                break;
+            default:
+                hand.type = HIGH_CARD;
+        }
+
+        hands.push_back(hand);
+    }
+
+    int totalWinnings = 0;
+    vector<Hand> sorted;
+
+    for (Hand h : hands) {
+        int i=0;
+        for (; i<sorted.size(); i++) {
+            Hand h2 = sorted[i];
+            if (h.type < h2.type) break;
+            else if (h.type == h2.type) {
+                for (int j=0; j<5; j++) {
+                    if (priorityMap[h.cards[j]] < priorityMap[h2.cards[j]]) goto nextHand;
+                    else if (priorityMap[h.cards[j]] > priorityMap[h2.cards[j]]) break;
+                }
+            }
+        }
+        nextHand:;
+        sorted.insert(sorted.begin()+i, h);
+    }
+
+    for (int i=0; i<sorted.size(); i++) {
+        totalWinnings += sorted[i].bid * (i+1);
+    }
+
+    return totalWinnings;
+}
+
 int main() {
     //ifstream file("./input.example.txt");
     ifstream file("../../inputs/2023/07.txt");
@@ -94,7 +164,8 @@ int main() {
         string line;
         while (getline(file, line)) input.push_back(line);
 
-        cout << partOne() << endl;
+        cout << "Part 1: " << partOne() << endl;
+        cout << "Part 2: " << partTwo() << endl;
     } else {
         cout << "Unable to read file input" << endl;
     }
