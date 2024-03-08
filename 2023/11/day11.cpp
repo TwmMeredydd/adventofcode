d88d7b5446155b2aa0948c1b3b8eb322f27af295
TwmMeredydd <twmmeredydd@gmail.com>
2024-03-08 14:03:30 +0000

    Solve 2023-11.1

---
+++ 2023/11/day11.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <set>
+#include <algorithm>
+
+using namespace std;
+
+struct Galaxy {
+    int x, y;
+};
+
+vector<Galaxy> galaxies;
+set<int> empty_rows;
+set<int> empty_cols;
+
+int partOne() {
+    int total_steps = 0;
+
+    for (int i=0; i<galaxies.size(); i++) {
+        Galaxy* galaxy = &galaxies[i];
+        
+        int xOffset = 0;
+        for (int col : empty_cols) if (galaxy->x > col) xOffset++;
+        galaxy->x += xOffset;
+        
+        int yOffset = 0;
+        for (int row : empty_rows) if (galaxy->y > row) yOffset++;
+        galaxy->y += yOffset;
+    }
+
+    for (int i=0; i<galaxies.size(); i++) {
+        Galaxy galaxy = galaxies[i];
+        for (int j=i+1; j<galaxies.size(); j++) {
+            Galaxy galaxy2 = galaxies[j];
+            total_steps += abs(galaxy2.x - galaxy.x) + abs(galaxy2.y - galaxy.y);
+        }
+    }
+
+    return total_steps;
+}
+
+int main() {
+    //ifstream file("./input.example.txt");
+    ifstream file("../../inputs/2023/11.txt");
+
+    if (file.is_open()) {
+        string line;
+        int row = 0;
+        while (getline(file, line)) {
+            bool empty = true;
+            set<int> dots;
+            for (int col=0; col<line.length(); col++)
+                if (line[col] == '#') {
+                    galaxies.push_back({col, row});
+                    empty = false;
+                } else {
+                    dots.insert(col);
+                }
+            
+            if (empty) empty_rows.insert(row);
+            
+            if (empty_cols.empty()) {
+                copy(dots.begin(), dots.end(), inserter(empty_cols, empty_cols.begin()));
+            } else {
+                set<int> intersect;
+                set_intersection(dots.begin(), dots.end(), empty_cols.begin(), empty_cols.end(), inserter(intersect, intersect.begin()));
+                empty_cols = intersect;           
+            }
+
+            row++;
+        }
+
+        cout << "Part 1: " << partOne() << endl;
+
+    } else {
+        cout << "Unable to read input" << endl;
+    }
+    return 0;
+}
